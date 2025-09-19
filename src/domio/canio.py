0737574fad63f683c07241d1844e3d67c61d5b16
franckinux <contact@franck-barbenoire.fr>
2025-09-19 21:48:26 +0200

    restart the can interface if nothing is received for 30 s

--- src/domio/canio.py
+++ src/domio/canio.py
@@ -7,6 +7,7 @@ import can
 
 import domio.config as config
 from domio.utils import done_callback
+from domio.utils import exec_cmd
 
 _running = True
 _task = None
@@ -23,6 +24,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+def handle_exception(loop, context):
+    logger.debug(f"exception reason: {context['message']}")
+
+
 def init():
     global _bus
     global _notifier
@@ -32,13 +37,14 @@ def init():
     try:
         _bus = can.Bus(interface="socketcan", channel="can0")
     except can.exceptions.CanOperationError as exc:
-        logger.error(f"something when wrong when connectingg to can socket: {exc}")
+        logger.error(f"something when wrong when connecting to can socket: {exc}")
         return
 
     _reader = can.AsyncBufferedReader()
 
     listeners = [_reader]
     loop = asyncio.get_running_loop()
+    loop.set_exception_handler(handle_exception)
     _notifier = can.Notifier(_bus, listeners, loop=loop)
 
     if _task is None:
@@ -52,10 +58,16 @@ async def _can_task():
 
     logger.info("started")
 
+    counter = 0
     while _running:
         try:
             msg = await asyncio.wait_for(_reader.get_message(), 1)
         except asyncio.TimeoutError:
+            counter += 1
+            if counter == 30:
+                counter = 0
+                await exec_cmd("sudo systemctl restart can0-socket.service")
+                logger.info("can interface restarted")
             continue
 
         # print(f"{msg.arbitration_id:X}: {msg.data}")
